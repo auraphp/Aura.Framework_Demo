971017c142a47d2c7686549fa3da6323fab7c6ac
Paul M. Jones <pmjones88@gmail.com>
2012-11-17 11:10:31 -0600

    extract routes to its own file

--- config/default.php
+++ config/default.php
@@ -4,13 +4,5 @@
  */
 $loader->add('Aura\Demo\\', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'src');
 
-// set up a simple "hello world" route
-$di->get('router_map')->add('aura.demo.hello.index', '/hello', [
-    'values' => [
-        'controller' => 'aura.demo.hello',
-        'action' => 'index',
-    ],
-]);
-
 // mappings for the web controller factory
 $di->params['Aura\Framework\Web\Controller\Factory']['map']['aura.demo.hello'] = 'Aura\Demo\Web\Hello\Page';
