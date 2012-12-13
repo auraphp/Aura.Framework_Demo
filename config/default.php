34db84a46b56735703089695fbc5da658c4f2730
Paul M. Jones <pmjones88@gmail.com>
2012-12-13 17:09:40 -0600

    update config with routes

--- config/default.php
+++ config/default.php
@@ -9,3 +9,8 @@ $di->params['Aura\Framework\Web\Controller\Factory']['map']['aura.demo.hello'] =
 
 // mappings for the cli factory
 $di->params['Aura\Framework\Cli\Factory']['map']["$system/package/Aura.Demo/cli/hello"] = 'Aura\Demo\Cli\Hello\Command';
+
+// routes for the demo
+$di->params['Aura\Router\Map']['attach']['/'] = function () use ($system) {
+    return include "$system/package/Aura.Demo/routes.php";
+};
