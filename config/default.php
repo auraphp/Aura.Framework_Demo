89d60aadf100a0fd6953c59a207022b0d3d4dbe2
Paul M. Jones <pmjones88@gmail.com>
2013-01-29 09:43:41 -0600

    standardize config file

--- config/default.php
+++ config/default.php
@@ -1,16 +1,20 @@
 <?php
 /**
- * Package prefix for autoloader.
+ * Loader
  */
 $loader->add('Aura\Demo\\', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'src');
 
-// mappings for the web controller factory
-$di->params['Aura\Framework\Web\Controller\Factory']['map']['aura.demo.hello'] = 'Aura\Demo\Web\Hello\Page';
-
-// mappings for the cli factory
+/**
+ * Aura\Framework\Cli\Factory
+ */
 $di->params['Aura\Framework\Cli\Factory']['map']["$system/package/Aura.Demo/cli/hello"] = 'Aura\Demo\Cli\Hello\Command';
 
-// routes for the demo
-$di->params['Aura\Router\Map']['attach']['/'] = function () use ($system) {
-    return include "$system/package/Aura.Demo/routes.php";
-};
+/**
+ * Aura\Framework\Web\Controller\Factory
+ */
+$di->params['Aura\Framework\Web\Controller\Factory']['map']['aura.demo.hello'] = 'Aura\Demo\Web\Hello\Page';
+
+/**
+ * Aura\Router\Map
+ */
+$di->params['Aura\Router\Map']['attach']['/'] = $di->lazyRequire("$system/package/Aura.Demo/routes.php");
