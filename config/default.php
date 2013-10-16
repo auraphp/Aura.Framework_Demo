b5db555d6078ea94f25f36c53140969c922a3825
Paul M. Jones <pmjones88@gmail.com>
2013-10-16 10:17:19 -0500

    [CFG] Use the new $router for attaching routes

--- config/default.php
+++ config/default.php
@@ -5,6 +5,11 @@
 $loader->add('Aura\Framework_Demo\\', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'src');
 
 /**
+ * Router
+ */
+$router->attach('/', $di->lazyRequire("$system/package/Aura.Framework_Demo/routes.php"));
+
+/**
  * Aura\Framework\Cli\Factory
  */
 $di->params['Aura\Framework\Cli\Factory']['map']["$system/package/Aura.Framework_Demo/cli/hello"] = 'Aura\Framework_Demo\Cli\Hello\Command';
@@ -13,8 +18,3 @@ $di->params['Aura\Framework\Cli\Factory']['map']["$system/package/Aura.Framework
  * Aura\Framework\Web\Controller\Factory
  */
 $di->params['Aura\Framework\Web\Controller\Factory']['map']['aura.demo.hello'] = 'Aura\Framework_Demo\Web\Hello\Page';
-
-/**
- * Aura\Router\Map
- */
-$di->params['Aura\Router\Map']['attach']['/'] = $di->lazyRequire("$system/package/Aura.Framework_Demo/routes.php");
