d35d93467dce2a128609f98696819c4a16aa4864
Paul M. Jones <pmjones88@gmail.com>
2015-03-24 17:11:21 -0500

    soothe phpdoc

--- src/Aura/Framework_Demo/Cli/Hello/Command.php
+++ src/Aura/Framework_Demo/Cli/Hello/Command.php
@@ -1,32 +1,30 @@
 <?php
 /**
- * 
+ *
  * This file is part of the Aura project for PHP.
- * 
- * @package Aura.Framework_Demo
- * 
+ *
  * @license http://opensource.org/licenses/bsd-license.php BSD
- * 
+ *
  */
 namespace Aura\Framework_Demo\Cli\Hello;
 
 use Aura\Framework\Cli\AbstractCommand;
 
 /**
- * 
+ *
  * A simple CLI command to output "Hello, World!"
- * 
+ *
  * @package Aura.Framework_Demo
- * 
+ *
  */
 class Command extends AbstractCommand
 {
     /**
-     * 
+     *
      * Sends "Hello World!" to standard output.
-     * 
+     *
      * @return void
-     * 
+     *
      */
     public function action()
     {
--- src/Aura/Framework_Demo/Web/Hello/views/index.php
+++ src/Aura/Framework_Demo/Web/Hello/views/index.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * 
+ *
  * This file is part of Aura for PHP.
- * 
+ *
  * @package Aura.Framework_Demo
- * 
+ *
  * @license http://opensource.org/licenses/bsd-license.php BSD
- * 
+ *
  */
 echo "Hello World!";
 ?>
